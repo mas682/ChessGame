61c8dc9012863a00a8756549a85da97ad5798acc
Matthew Stropkey <mas682@pitt.edu>
2020-02-23 13:24:48 -0500

    finished logic for vertical, horizontal, and knight L shaped move

--- script.js
+++ script.js
@@ -1,29 +1,294 @@
 
+// holds which blocks have currently been clicked on
+let flashing = []
+// used to synchronzie block color change
+let flip_color = false
 
-
-function update(e, square) {
+// called when a block is clicked on
+// if the block was already clicked on, remove it from flashing
+// otherwise, add it to the set of flashing blocks
+function setFlash(e, elem) {
     e.preventDefault();
-    elem = document.getElementById('one');
-    square.style.background = "blue";
-
-}
-
-
-
-elem = document.getElementById('one')
-elem.addEventListener("click", function(e){update(e, this);}, false);
-
-function flash()
-{
-    elem = document.getElementById('one');
-    if(elem.style.background === 'blue')
+    let index = flashing.indexOf(elem.id);
+    if(index < 0)
     {
-        elem.style.background = 'black';
+        flashing.push(elem.id);
     }
     else
     {
-        elem.style.background = 'blue';
+        flashing.splice(index, 1);
+        if(elem.style.backgroundColor == "red")
+        {
+            if(elem.className === "square even")
+            {
+                elem.style.backgroundColor = "grey";
+            }
+            else
+            {
+                elem.style.backgroundColor = "white";
+            }
+        }
     }
+    // diagonalFlash(elem.id);
+    //verticalFlash(elem.id);
+    //horizontalFlash(elem.id);
+    knightLMove(elem.id);
+}
+
+// function to manage making blocks switch colors
+function flash()
+{
+    // switch between true and false
+    flip_color = !flip_color
+
+    let i = 0
+    // go through each flashing block number
+    for(i = 0; i < flashing.length; i++)
+    {
+        elem = document.getElementById(flashing[i]);
+        // if blocks color is not red and it should be flashing, set it
+        if(elem.style.backgroundColor !== "red" && flip_color)
+        {
+            elem.style.backgroundColor = "red";
+        }
+        // if the block should not be red currently
+        else
+        {
+            if(elem.className == "square even")
+            {
+                elem.style.backgroundColor = "black";
+            }
+            else
+            {
+                elem.style.backgroundColor = "white";
+            }
+        }
+    }
+}
+
+// function to handle getting diagonals
+function diagonalFlash(pos)
+{
+    pos = parseInt(pos);
+    let p = pos;
+    let squares_flashing = [];
+    let previous_pos = pos % 8;
+    // for up, left
+    p = p - 9;
+    while(p >= 0 && previous_pos != 0)
+    {
+        squares_flashing.push(p);
+        // get position within row
+        previous_pos = p % 8;
+        // update value
+        p = p - 9;
+    }
+    // reset values
+    // for up, right
+    p = pos;
+    previous_pos = pos % 8;
+    p = p - 7;
+    while(p >= 0 && previous_pos != 7)
+    {
+        squares_flashing.push(p);
+        // get position within row
+        previous_pos = p % 8;
+        p = p - 7;
+    }
+    // reset values
+    // for down, left
+    p = pos;
+    previous_pos = pos % 8;
+    p = p + 7;
+    while(p < 64 && previous_pos != 0)
+    {
+        squares_flashing.push(p);
+        // get position within row
+        previous_pos = p % 8;
+        // update value
+        p = p + 7;
+    }
+    // reset values
+    // for down, right
+    p = pos;
+    previous_pos = pos % 8;
+    p = p + 9;
+    while(p < 64 && previous_pos != 7)
+    {
+        squares_flashing.push(p);
+        // get position within row
+        previous_pos = p % 8;
+        // update value
+        p = p + 9;
+    }
+    squares_flashing.push(pos);
+    flashing = squares_flashing;
+    console.log(squares_flashing);
+    // have issue where you can change what is flashing without resetting squares that were flashing
+    // will want to unset all squares when calling setFlash
+}
+
+// function to get verticals
+function verticalFlash(pos)
+{
+    pos = parseInt(pos);
+    let p = pos;
+    let squares_flashing = [];
+    // for up
+    p = p - 8;
+    while(p >= 0)
+    {
+        squares_flashing.push(p);
+        // update value
+        p = p - 8;
+    }
+    // for down
+    p = pos;
+    p = p + 8;
+    while(p < 64)
+    {
+        squares_flashing.push(p);
+        // update value
+        p = p + 8;
+    }
+    squares_flashing.push(pos);
+    flashing = squares_flashing;
+    console.log(squares_flashing);
+}
+
+// function to get horizontals
+function horizontalFlash(pos)
+{
+    pos = parseInt(pos);
+    let p = pos;
+    let squares_flashing = [];
+    let previous_pos = pos % 8;
+    // for left
+    p = p - 1;
+    while(previous_pos != 0)
+    {
+        squares_flashing.push(p);
+        // get position within row
+        previous_pos = p % 8;
+        // update value
+        p = p - 1;
+    }
+    // reset values
+    // for right
+    p = pos;
+    previous_pos = pos % 8;
+    p = p + 1;
+    while(previous_pos != 7)
+    {
+        squares_flashing.push(p);
+        // get position within row
+        previous_pos = p % 8;
+        p = p + 1;
+    }
+    squares_flashing.push(pos);
+    flashing = squares_flashing;
+    console.log(squares_flashing);
+}
+
+function knightLMove(pos)
+{
+    pos = parseInt(pos);
+    let p = pos;
+    let squares_flashing = [];
+    let row_pos = pos % 8;
+    // for two up, one left
+    // if alread at left most column, skip
+    if(row_pos > 0)
+    {
+        p = p - 17;
+        if(p >= 0)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    // one up, two left
+    p = pos;
+    if(row_pos > 1)
+    {
+        p = p - 10;
+        if(p >= 0)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    // two up, one right
+    p = pos;
+    if(row_pos < 7)
+    {
+        p = p - 15;
+        if(p >= 0)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    // one up, two right
+    p = pos;
+    if(row_pos < 6)
+    {
+        p = p - 6;
+        if(p >= 0)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    // two down, one right
+    p = pos;
+    if(row_pos < 7)
+    {
+        p = p + 17;
+        if(p < 64)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    // one down, two right
+    p = pos;
+    if(row_pos < 6)
+    {
+        p = p + 10;
+        if(p < 64)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    // one down, two left
+    p = pos;
+    if(row_pos > 1)
+    {
+        p = p + 6;
+        if(p < 64)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    // two, one left
+    p = pos;
+    if(row_pos > 0)
+    {
+        p = p + 15;
+        if(p < 64)
+        {
+            squares_flashing.push(p);
+        }
+    }
+    squares_flashing.push(pos);
+    flashing = squares_flashing;
+    console.log(squares_flashing);
+}
+
+
+// get all the blocks
+elems = document.querySelectorAll(".square");
+let i = 0;
+for(i = 0; i < elems.length; i++)
+{
+    // add a click listener to each block
+    elems[i].addEventListener('click', function(e){ setFlash(e, this); }, false);
 }
 
 setInterval(flash, 1000);
